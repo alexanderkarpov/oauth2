f0359f88138022fe4ed813c68ce7b0087ac2f1d5
akarpov <alexander.karpoff@gmail.com>
2016-07-15 14:29:35 +0300

    security

--- src/main/java/com/example/auth/Oauth2AuthorizationServerApplication.java
+++ src/main/java/com/example/auth/Oauth2AuthorizationServerApplication.java
@@ -2,11 +2,17 @@ package com.example.auth;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -25,6 +31,26 @@ public class Oauth2AuthorizationServerApplication extends WebMvcConfigurerAdapte
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
+    }
+
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory().withClient("foo").secret("foosecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid")
+                    .authorities("ROLE_USER", "ROLE_TEST");
+        }
     }
 
 }
